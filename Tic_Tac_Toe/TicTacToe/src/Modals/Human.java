34bad729c4b9366c23b60e49d67704829dd651f4
RjFuture <rajnishkumar4999@gmail.com>
2026-08-08 13:24:18 +0530

    solved the bug as the same move overwriting the old move at same place

--- Tic_Tac_Toe/TicTacToe/src/Modals/Human.java
+++ Tic_Tac_Toe/TicTacToe/src/Modals/Human.java
@@ -1,5 +1,6 @@
 package Modals;
 
+import Modals.enums.CellState;
 import Modals.enums.PlayerType;
 
 import java.util.Scanner;
@@ -18,9 +19,33 @@ public class Human extends Player{
     public Move makeMove(Game game)
     {
        System.out.println("tell your move "+this.getName()+" by telling which row and which column");
+
+
        int row = sc.nextInt();
        int col = sc.nextInt();
+       while(!validateMove(row,col,game))
+       {
+           System.out.println("please try again");
+           row = sc.nextInt();
+           col = sc.nextInt();
+       }
 
        return  new Move(this, new Cell(row,col));
+    }
+
+    private boolean validateMove(int row, int col,Game game) {
+        int sizeOfBoard = game.getBoard().getSize();
+        if(sizeOfBoard<=row || sizeOfBoard<=col || row<0 || col<0)
+        {
+            System.out.println("Out of bounds move ");
+            return false;
+        }
+
+        if(game.getBoard().getCells().get(row).get(col).getCellstate().equals(CellState.FILLED))
+        {
+            System.out.println("Wrong move the cell is filled already");
+            return false;
+        }
+        return true;
     }
 }
---
+++ Tic_Tac_Toe/TicTacToe/src/ReadMe.txt
@@ -0,0 +1,7 @@
+//Requirements
+
+
+1.) we can make like that if it is player vs player we can try to do check first if cell is filled then move will be invalid
+
+2.) input should be take like name , symbol , age from client just one time and we can make factory for creating object for player
+like factory pattern and add in list of players from our side
